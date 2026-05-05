8043f2a42ae516ab3f0e5eaf56784c20d2f80593
bwhtsu <liliiyazh5@gmail.com>
2026-05-05 19:22:12 +0300

    home work

--- src/app.js
+++ src/app.js
@@ -25,13 +25,13 @@ const id = setInterval(() => {
 const colorEl = document.querySelector(".block1");
 const sizeEl = document.querySelector(".block2");
 
-let size = 0;
+let size = 10;
 
 setInterval(() => {
 
-    let r = Math.round(Math.random() * (255 - 1) * 1);
-    let g = Math.round(Math.random() * (255 - 1) * 1);
-    let b = Math.round(Math.random() * (255 - 1) * 1);
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
 
     colorEl.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
 
@@ -42,8 +42,91 @@ setInterval(() => {
 
 },1000);
 
+
+console.log(counterEl, colorEl, sizeEl);
 // =================================================================
 
 // Завдання 3
 
 // Завдання "Інтерактивна гра": Створіть просту інтерактивну гру, де гравець має натискати на елементи на сторінці протягом певного інтервалу часу, використовуючи setInterval. Реалізуйте лічильник очок та відслідковуйте кількість натисків гравця.
+
+const playfield = document.querySelector(".game");
+const bolEl = document.querySelector(".bol");
+const numberEl = document.querySelector(".number");
+
+let score = 0;
+let time = 30; 
+let gameInterval;
+let moveInterval;
+
+
+function moveBall() {
+    const maxX = playfield.clientWidth - bolEl.clientWidth;
+    const maxY = playfield.clientHeight - bolEl.clientHeight;
+
+    const x = Math.floor(Math.random() * maxX);
+    const y = Math.floor(Math.random() * maxY);
+
+    bolEl.style.left = x + "px";
+    bolEl.style.top = y + "px";
+}
+
+
+bolEl.addEventListener("click", () => {
+    if (time > 0) {
+        score++;
+        numberEl.textContent = score;
+    }
+});
+
+
+function startGame() {
+
+    moveInterval = setInterval(() => {
+        moveBall();
+    }, 800);
+
+    gameInterval = setInterval(() => {
+        time--;
+
+        if (time <= 0) {
+            clearInterval(gameInterval);
+            clearInterval(moveInterval);
+
+            alert("Гра завершена! Ваш результат: " + score);
+        }
+
+    }, 1000);
+}
+
+moveBall();
+startGame();
+
+
+
+
+// =================================================================
+
+// Завдання 4
+
+// Завдання "Контроль часу": Створіть програму, яка дозволяє користувачу встановити певний час (у секундах) за допомогою введення з клавіатури. Потім використовуйте setTimeout або setInterval, щоб після встановленого часу вивести повідомлення.
+
+const timeInput = document.querySelector(".time");
+const btnTime = document.querySelector(".btn-time");
+
+btnTime.addEventListener("click", () => {
+    let time = Number(timeInput.value);
+
+    if (!time || time <= 0) {
+        alert("Введи правильний час у секундах!");
+        return;
+    }
+
+    let timeMs = time * 1000;
+
+    setTimeout(() => {
+        alert("Час вийшов!");
+    }, timeMs);
+});
+
+
